a17efc2e40e00b6a2a32420182d30c7d6db1feee
Aondre <aondref@gmail.com>
2021-07-20 18:35:05 -0400

    React click counter

--- Version03/client/src/Pages/HomePage/Home.js
+++ Version03/client/src/Pages/HomePage/Home.js
@@ -1,7 +1,19 @@
+import React, { useState, useEffect } from 'react'
+
 const Home = () => {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        document.title = `You clicked ${count} times`;
+    });
+
     return(
         <div>
             <h1>Hello World</h1>
+            <p>You clicked {count} times.</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click Me
+            </button>
         </div>
     )
 }
